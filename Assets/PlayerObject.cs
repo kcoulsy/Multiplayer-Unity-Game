386d35d52885fd1845239578919f8e5f17c503f5
Kristian <kristian@coulsy.co.uk>
2018-11-27 19:06:46 +0000

    Initial commit of Unity Files

---
+++ Assets/PlayerObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerObject : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		if (!isLocalPlayer) {
+			return;
+		}
+
+		Debug.Log("PlayerObject::Start -- Spawning my own personal unit");
+
+	}
+	
+	public GameObject PlayerUnitPrefab;
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
