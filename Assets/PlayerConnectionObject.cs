414c536a6986d35787583eee3d19a0b52530abcd
Kristian <kristian@coulsy.co.uk>
2018-11-27 19:50:54 +0000

    Spawn unit and basic client side control

---
+++ Assets/PlayerConnectionObject.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerConnectionObject : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		if (!isLocalPlayer) {
+			return;
+		}
+
+		Debug.Log("PlayerObject::Start -- Spawning my own personal unit");
+
+		CmdSpawnMyUnit();
+
+	}
+	
+	public GameObject PlayerUnitPrefab;
+
+	// Update is called once per frame
+	void Update () {
+		// Runs on everyones componuter regardless of who's object it is
+		if (Input.GetKeyDown(KeyCode.S)) {
+			CmdSpawnMyUnit();
+		}
+	}
+
+	/**  		#### COMMANDS ####
+	*
+	* 	Commands are functions that ONLY get called by the server
+	* 	Must have [Command] before and start with Cmd
+	*/
+
+	[Command]
+	void CmdSpawnMyUnit() {
+
+		GameObject go = Instantiate(PlayerUnitPrefab);
+
+		NetworkServer.SpawnWithClientAuthority(go, connectionToClient);
+	}
+
+}
