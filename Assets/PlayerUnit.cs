89f546d4dec0cd27b593a9115c473d3c759339dd
Kristian <kristian@coulsy.co.uk>
2019-01-28 19:55:22 +0000

    Updated movement controls and prefab

--- Assets/PlayerUnit.cs
+++ Assets/PlayerUnit.cs
@@ -5,23 +5,28 @@ using UnityEngine.Networking;
 
 public class PlayerUnit : NetworkBehaviour {
 
-	public float speed;
-	public float jumpForce;
-	private float moveInput;
-	private Rigidbody2D rb;
-	private bool facingRight = true;
+	private Transform  playerCamera, centerPoint, player;
 
-	private bool isGrounded;
-	public Transform groundCheck;
-	public float checkRadius;
-	public LayerMask whatIsGround;
+	private float mouseX, mouseY;
+	public float mouseSensitivity = 3f;
+	public float mouseYPosition = 1f;
 
-	public int extraJumpsValue;
-	private int extraJumps = 0;
+	private float moveFB, moveLR;
+	public float moveSpeed = 2f;
+
+	private float zoom;
+	public float zoomSpeed = 2;
+	public float zoomMin = -2f;
+	public float zoomMax = -10f;
+
+	public float rotationSpeed = 2f;
 
 	// Use this for initialization
 	void Start () {
-		rb = GetComponent<Rigidbody2D>();
+		playerCamera = GameObject.Find("Camera").transform;
+		centerPoint = GameObject.Find("CameraTarget").transform;
+		player = gameObject.transform;
+		zoom = -5;
 	}
 	
 	// Update is called once per frame
@@ -29,45 +34,49 @@ public class PlayerUnit : NetworkBehaviour {
 		if (!hasAuthority) {
 			return;
 		}
-		Debug.Log(isGrounded);
-		if (isGrounded) {
-			extraJumps = extraJumpsValue;
-		}
+		ZoomCamera();
+		RotateCamera();
+		Move();
 
-		if (Input.GetKeyDown(KeyCode.Space) && extraJumps > 0) {
-			rb.velocity = Vector2.up * jumpForce;
-			extraJumps--;
-		} else if (Input.GetKeyDown(KeyCode.Space) && extraJumps == 0) {
-			rb.velocity = Vector2.up * jumpForce;
-		}
-
-		
-		// if (Input.GetKeyDown(KeyCode.D)) {
-		// 	Destroy(gameObject);
-		// }
-		
 	}
 
-	void FixedUpdate() {
-		if (!hasAuthority) {
-			return;
+	void ZoomCamera() {
+		zoom += Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+		if ( zoom > zoomMin ) {
+			zoom = zoomMin;
 		}
-
-		isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
-		moveInput = Input.GetAxis("Horizontal");
-		rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
-
-		if (!facingRight && moveInput > 0) {
-			Flip();
-		} else if (facingRight && moveInput < 0){
-			Flip();
+		if ( zoom < zoomMax ) {
+			zoom = zoomMax;
 		}
+		playerCamera.transform.localPosition = new Vector3(0, 0, zoom);
 	}
 
-	void Flip() {
-		facingRight = !facingRight;
-		Vector3 Scaler = transform.localScale;
-		Scaler.x *= -1;
-		transform.localScale = Scaler;
+	void RotateCamera() {
+		// Right mouse click
+		if (Input.GetMouseButton(1)) {
+			mouseX += Input.GetAxis("Mouse X") * mouseSensitivity;
+			mouseY -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+		}
+		mouseY = Mathf.Clamp(mouseY, -30f, 85f);
+
+		playerCamera.LookAt(centerPoint);
+		centerPoint.localRotation = Quaternion.Euler(mouseY, mouseX, 0);
+	}
+
+	void Move() {
+		//Handle wasd movement
+		moveFB = Input.GetAxis("Vertical") * moveSpeed;
+		moveLR = Input.GetAxis("Horizontal") * moveSpeed;
+
+		Vector3 movement = new Vector3(moveLR, 0, moveFB);
+		movement = player.rotation * movement;
+
+		player.GetComponent<CharacterController>().Move(movement * Time.deltaTime);
+		centerPoint.position = new Vector3(player.position.x, player.position.y, player.position.z);
+
+		if (Input.GetAxis("Vertical") != 0) {
+			Quaternion turnAngle = Quaternion.Euler(0, centerPoint.eulerAngles.y, 0);
+			player.rotation = Quaternion.Slerp(player.rotation, turnAngle, Time.deltaTime * rotationSpeed);
+		}
 	}
 }
